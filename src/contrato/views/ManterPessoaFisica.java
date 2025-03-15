c14683d551f6fbd1979af4c3cee9474b6e70a477
Arthur Selingin <arthur_santos@outlook.com.br>
2025-03-15 20:51:51 -0300

    manter pessoa fisica feito

--- src/contrato/views/ManterPessoaFisica.java
+++ src/contrato/views/ManterPessoaFisica.java
@@ -2,8 +2,11 @@ package contrato.views;
 
 import javax.swing.JOptionPane;
 
-public class ManterPessoaFisica implements ContratoInteface {
+import contrato.controllers.ControllerPessoaFisica;
+import contrato.model.dados.PessoaFisica;
 
+public class ManterPessoaFisica implements ContratoInteface {
+    
     @Override
    public void menu() {
         String msg = "Entre com a opção \n";
@@ -27,30 +30,54 @@ public class ManterPessoaFisica implements ContratoInteface {
 
     @Override
     public void alterar() {
-        
-        
+        System.out.println("Opção Alterar");
+        String nome = JOptionPane.showInputDialog("NOME");
+        String cpf = JOptionPane.showInputDialog("CPF");
+        int id = Integer.parseInt(JOptionPane.showInputDialog("ID"));
+        ControllerPessoaFisica cPessoaFisica = new ControllerPessoaFisica();
+        PessoaFisica entradaPessoaFisica = new PessoaFisica(id, nome, cpf);
+        PessoaFisica saidaPessoaFisica = (PessoaFisica) cPessoaFisica.Alterar(entradaPessoaFisica);
+        JOptionPane.showMessageDialog(null, saidaPessoaFisica);
+
     }
 
     @Override
     public void buscar() {
-        // TODO Auto-generated method stub
+        System.out.println("Opção Buscar");
+        int id = Integer.parseInt(JOptionPane.showInputDialog(null, "ID"));
+        ControllerPessoaFisica cPessoaFisica = new ControllerPessoaFisica();
+        PessoaFisica saidaPessoaFisica =  (PessoaFisica) cPessoaFisica.Buscar(id);
+        JOptionPane.showMessageDialog(null, saidaPessoaFisica);
         
     }
 
     @Override
     public void excluir() {
-        // TODO Auto-generated method stub
-        
+        System.out.println("Opção Excluir");
+        int id = Integer.parseInt(JOptionPane.showInputDialog(null, "ID"));
+        ControllerPessoaFisica cPessoaFisica = new ControllerPessoaFisica();
+        Boolean saidaPessoaFisica = cPessoaFisica.Excluir(id);
+        JOptionPane.showMessageDialog(null, saidaPessoaFisica);
     }
 
     @Override
     public void inserir() {
-        // TODO Auto-generated method stub
+        System.out.println("Opção Inserir");
+        ControllerPessoaFisica cPessoaFisica = new ControllerPessoaFisica();
+        
+        String nome = JOptionPane.showInputDialog(null, "Nome");
+        String cpf = JOptionPane.showInputDialog(null, "CPF");
+        int id = Integer.parseInt(JOptionPane.showInputDialog(null, "ID")); 
+
+        PessoaFisica entradaPessoaFisica = new PessoaFisica(id, nome, cpf);
+        PessoaFisica saidaPessoaFisica = (PessoaFisica) cPessoaFisica.Inserir(entradaPessoaFisica);
+        JOptionPane.showMessageDialog(null, saidaPessoaFisica);
         
     }
 
     @Override
     public void listar() {
+        System.out.println("Opção listar");
         // TODO Auto-generated method stub
         
     }

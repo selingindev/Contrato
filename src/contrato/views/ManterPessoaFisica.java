909b0b91935e67729bef8f95034c6489980043cb
Arthur Selingin <arthur_santos@outlook.com.br>
2025-03-14 22:38:42 -0300

    botando menu no topo

--- src/contrato/views/ManterPessoaFisica.java
+++ src/contrato/views/ManterPessoaFisica.java
@@ -5,8 +5,29 @@ import javax.swing.JOptionPane;
 public class ManterPessoaFisica implements ContratoInteface {
 
     @Override
+   public void menu() {
+        String msg = "Entre com a opção \n";
+        msg += "0. Fim \n";
+        msg += "1. Inserir \n";
+        msg += "2. Alterar \n";
+        msg += "3. Excluir \n";
+        msg += "4. Listar \n";
+        msg += "5. Buscar ";
+        int opcao = Integer.parseInt(JOptionPane.showInputDialog(msg));
+    
+        switch(opcao){
+            case 1 -> inserir();
+            case 2 -> alterar();
+            case 3 -> excluir();
+            case 4 -> listar();
+            case 5 -> buscar();
+            default -> System.out.println("Opção inválida.");
+        }
+    }
+
+    @Override
     public void alterar() {
-        // TODO Auto-generated method stub
+        
         
     }
 
@@ -32,27 +53,6 @@ public class ManterPessoaFisica implements ContratoInteface {
     public void listar() {
         // TODO Auto-generated method stub
         
-    }
-
-    @Override
-   public void menu() {
-        String msg = "Entre com a opção \n";
-        msg += "0. Fim \n";
-        msg += "1. Inserir \n";
-        msg += "2. Alterar \n";
-        msg += "3. Excluir \n";
-        msg += "4. Listar \n";
-        msg += "5. Buscar ";
-        int opcao = Integer.parseInt(JOptionPane.showInputDialog(msg));
-    
-        switch(opcao){
-            case 1 -> inserir();
-            case 2 -> alterar();
-            case 3 -> excluir();
-            case 4 -> listar();
-            case 5 -> buscar();
-            default -> System.out.println("Opção inválida.");
-        }
     }
 
 }

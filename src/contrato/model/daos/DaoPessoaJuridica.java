5929c397502ef435d737ddd30f870db8b35567f4
Arthur Selingin <arthur_santos@outlook.com.br>
2025-03-14 22:38:27 -0300

    dao pessoa juridica

---
+++ src/contrato/model/daos/DaoPessoaJuridica.java
@@ -0,0 +1,44 @@
+package contrato.model.daos;
+
+import java.util.List;
+
+public class DaoPessoaJuridica implements ContratoDaos {
+
+    @Override
+    public Object Buscar(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Buscar'");
+    }
+
+    @Override
+    public Object Inserir(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Inserir'");
+    }
+
+    @Override
+    public Object Alterar(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Alterar'");
+    }
+
+    @Override
+    public Boolean Excluir(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Excluir'");
+    }
+
+    @Override
+    public List<Object> Listar(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Listar'");
+    }
+
+    @Override
+    public List<Object> Listar(String param) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Listar'");
+    }
+
+    
+}

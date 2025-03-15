8752161a749cffc28759f84e640519c1d6168d71
Arthur Selingin <arthur_santos@outlook.com.br>
2025-03-14 22:39:07 -0300

    começando inserir na interface da pessoa juridica

---
+++ src/contrato/views/ManterPessoaJuridica.java
@@ -0,0 +1,68 @@
+package contrato.views;
+
+import javax.swing.JOptionPane;
+
+import contrato.controllers.ControllerPessoaFisica;
+import contrato.model.dados.PessoaFisica;
+
+public class ManterPessoaJuridica implements ContratoInteface{
+
+   @Override
+   public void menu() {
+        String msg = "Entre com a opção \n";
+        msg += "0. Fim \n";
+        msg += "1. Inserir \n";
+        msg += "2. Alterar \n";
+        msg += "3. Excluir \n";
+        msg += "4. Listar \n";
+        msg += "5. Buscar ";
+        int opcao = Integer.parseInt(JOptionPane.showInputDialog(msg));
+    
+        switch(opcao){
+            case 1 -> inserir();
+            case 2 -> alterar();
+            case 3 -> excluir();
+            case 4 -> listar();
+            case 5 -> buscar();
+            default -> System.out.println("Opção inválida.");
+        }
+    }
+
+    @Override
+    public void inserir() {
+      String nome =  JOptionPane.showInputDialog("Nome");
+      String cpf = JOptionPane.showInputDialog("cpf");
+      int id = Integer.parseInt(JOptionPane.showInputDialog("id"));
+        
+      ControllerPessoaFisica cPessoaFisica = new ControllerPessoaFisica();
+      PessoaFisica entradaPF =  new PessoaFisica(id, nome, cpf);
+      PessoaFisica saidaPF = (PessoaFisica) cPessoaFisica.Inserir(entradaPF);
+      JOptionPane.showMessageDialog(null, saidaPF);
+    }
+
+    @Override
+    public void alterar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'alterar'");
+    }
+
+    @Override
+    public void excluir() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'excluir'");
+    }
+
+    @Override
+    public void listar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+    }
+
+    @Override
+    public void buscar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscar'");
+    }
+
+    
+}
